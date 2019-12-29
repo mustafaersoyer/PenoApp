a0f39bbf323deb6cf02f94f43192a25a42d6cc3c
Mustafa Ersoy <40203872+mustafaersoyer@users.noreply.github.com>
2019-12-29 22:31:07 +0300

    OneSignal added.

--- app/src/main/java/com/example/penoapp/MainActivity.java
+++ app/src/main/java/com/example/penoapp/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.penoapp.ui.notices.NoticesFragment;
+import com.onesignal.OneSignal;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
         setContentView(R.layout.main_activity);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
